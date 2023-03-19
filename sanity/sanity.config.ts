a1b2cd3f61baff7ee34023ec9d98167cd5f73976
zahid hussain <zahidhusssainzahid43@gmail.com>
2023-03-19 16:35:28 +0500

    Update sanity.config.ts

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
-import { visionTool } from '@sanity/vision';
 import schemaTypes from './schemaTypes'
 export default defineConfig({
   name: 'default',
