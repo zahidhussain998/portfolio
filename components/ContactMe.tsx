e3fdc8b98903f51d70f6eaf15c8897d35de3864b
zahid hussain <zahidhusssainzahid43@gmail.com>
2023-03-25 02:14:24 +0500

    Update ContactMe.tsx

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -40,7 +40,7 @@ const ContactMe = (props: Props) => {
           </div>
           <div className="flex items-center space-x-5 justify-center">
             <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">Pakistan Hyderabad Sindh</p>
+            <p className="text-2xl">Pakistan</p>
           </div>
           <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
